c5473dc87751f0ece81692106ce7a7652fe8125d
Duncan Kioi <duncan@griffinkenya.com>
2020-04-12 00:40:20 +0300

    Math.trunc was removed

--- src/estimator.js
+++ src/estimator.js
@@ -33,12 +33,12 @@ const displayData = (input) => {
   }
   const currentlyInfectedImpact = impactTotal * (2 ** Math.trunc(timetoElapse / 3));
   const currentlyInfectedSevere = severeImpactTotal * (2 ** Math.trunc(timetoElapse / 3));
-  const severeCasesByRequestedTimeImpact = Math.trunc((15 / 100) * currentlyInfectedImpact);
+  const severeCasesByRequestedTimeImpact = (15 / 100) * currentlyInfectedImpact;
   const numberofhospitalbeds = Math.trunc(hospitalbeds * (35 / 100));
   const hospitalBedsByRequestedTimeImpact = numberofhospitalbeds - severeCasesByRequestedTimeImpact;
   const casesForICUByRequestedTimeImpact = Math.trunc((5 / 100) * currentlyInfectedImpact);
   const casesForVentilatorsByRequestedTimeImpact = Math.trunk((2 / 100) * currentlyInfectedImpact);
-  const severeCasesByRequestedTimeSevere = Math.trunc((15 / 100) * currentlyInfectedSevere);
+  const severeCasesByRequestedTimeSevere = (15 / 100) * currentlyInfectedSevere;
   const hospitalBedsByRequestedTimeSevere = numberofhospitalbeds - severeCasesByRequestedTimeSevere;
   const casesForICUByRequestedTimeSevere = Math.trunc((5 / 100) * currentlyInfectedSevere);
   const casesForVentilatorsByRequestedTimeSevere = Math.trunc((2 / 100) * currentlyInfectedSevere);
